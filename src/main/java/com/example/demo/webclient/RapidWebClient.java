995ce58c13f497900e99b3945c1059cc49bc2437
Szymon Korda <szymonkrd@gmail.com>
2024-06-21 16:19:05 +0200

    Fetching one league's teams v2

--- src/main/java/com/example/demo/webclient/RapidWebClient.java
+++ src/main/java/com/example/demo/webclient/RapidWebClient.java
@@ -1,6 +1,7 @@
 package com.example.demo.webclient;
 
 import com.example.demo.payload.TeamInformationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,25 +10,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
-public class RapidWebClient {
+public class RapidWebClient{
     @Value("${api_key}")
     private String apiKey;
 
-    private final WebClient webClient;
-
-    public RapidWebClient() {
-        this.webClient = WebClient.builder()
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader("x-rapidapi-key", apiKey)
-                .defaultHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com")
-                .build();
-    }
-
     public TeamInformationResponse fetchTeams() {
+        WebClient webClient = prepareWebClient();
         Mono<TeamInformationResponse> mono = webClient.get()
                 .uri("https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=2023")
                 .retrieve()
                 .bodyToMono(TeamInformationResponse.class);
         return mono.block();
+    }
+
+    //TODO: skorda Find way how to autowire web client as dependency
+    private WebClient prepareWebClient() {
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("x-rapidapi-key", apiKey)
+                .defaultHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com")
+                .build();
     }
 }
